f0fb911f7485b3c6ec0759cd19f3dbd82125553c
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-19 08:48:11 +0600

    all toys api created

--- index.js
+++ index.js
@@ -28,8 +28,16 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
+
+    const toyCollection = client.db('drivenDB').collection('toys');
+
+    app.get('/toys', async(req,res)=>{
+        const result = await toyCollection.find().toArray();
+        res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
