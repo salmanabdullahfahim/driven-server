5b3836a8b06f06526fae9131ec4f54fb7f716e47
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-19 10:56:55 +0600

    sigle toy api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -35,6 +35,13 @@ async function run() {
 
     app.get('/toys', async(req,res)=>{
         const result = await toyCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.get('/toys/id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await toyCollection.findOne(query);
         res.send(result);
     })
 
