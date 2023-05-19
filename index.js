a558eabd2fe0d2b937b933b842d3cc4dada3f048
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-19 11:05:02 +0600

    sigle data api problem fixed

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
         res.send(result);
     })
 
-    app.get('/toys/id', async(req,res)=>{
+    app.get('/toys/:id', async(req,res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
         const result = await toyCollection.findOne(query);
