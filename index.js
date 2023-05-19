b7e170d4832142fc99106bce7731e6ab6f92de9a
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2023-05-19 14:58:09 +0600

    category wise data api creation done

--- index.js
+++ index.js
@@ -38,10 +38,20 @@ async function run() {
         res.send(result);
     })
 
+    //filter by id
+
     app.get('/toys/:id', async(req,res)=>{
         const id = req.params.id;
         const query = {_id: new ObjectId(id)};
         const result = await toyCollection.findOne(query);
+        res.send(result);
+    })
+
+    //filter by category
+    
+    app.get('/toys/:category', async (req,res)=>{
+        const subCategory = req.params.category;
+        const result = await toyCollection.find({subCategory: subCategory}).toArray();
         res.send(result);
     })
 
